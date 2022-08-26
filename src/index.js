99f72cab0d742c4f11fd21ad1783c6b24a9a9ac3
nethalayaswanth <yaswanthnethala1999@gmail.com>
2022-08-26 08:08:29 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    csrfPrevention: true,
-    cors: {
-      origin: [
-        "https://monyflix.vercel.app",
-        "http://localhost:3000/",
-        "https://studio.apollographql.com",
-      ],
-    },
+    // csrfPrevention: true,
+    // cors: {
+    //   origin: [
+    //     "https://monyflix.vercel.app",
+    //     "http://localhost:3000/",
+    //     "https://studio.apollographql.com",
+    //   ],
+    // },
     dataSources: () => {
       return {
         MovieAPI: new MovieAPI(),
